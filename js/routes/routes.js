bc60be5a1ea78a4f7e91c84f01b3e2202fa93200
Michael Smith <mijamessmith@gmail.com>
2020-08-11 15:45:54 -0700

    continued build work

--- js/routes/routes.js
+++ js/routes/routes.js
@@ -10,6 +10,34 @@ router.get('/login', function (req, res, next) {
     res.render('pages/login');
 });
 
+router.post("/loginUser", async (req, res, next) => {
+    var message;
+    console.log(req.body)
+    console.log(`Sent in a post using email: ${req.body.teacherEmail} and password: ${req.body.teacherPassword}.`)
+    try {
+            db.pool.query(`SELECT * FROM user_data WHERE email = "${req.body.teacherEmail}" AND password = "${req.body.teacherPassword}"`, (error, results) => {
+                if (error) {
+                    console.log(error);
+                } else if (results.length === 0) {
+                    message = { message: "The email or password is incorrect" }
+                    console.log(message);
+                    res.status(401).render("pages/login", message)
+                } else if (results.length > 0) {               
+                    req.session.currentUser = results[0];
+                    req.session.currentUserId = results[0].id;
+                    console.log(req.session.currentUser, req.session.currentUserId)
+                    res.render("pages/teachers");
+                }
+            })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+
+
+
 router.get('/register', function (req, res, next) {
     res.render('pages/register');
 });
@@ -17,6 +45,11 @@ router.get('/register', function (req, res, next) {
 router.get('/students', function (req, res, next) {
     res.render('pages/students');
 });
+
+router.get("/teachers", function (req, res, next) {
+    res.render("pages/teachers");
+})
+
 
 router.post("/registerUser", (req, res, next) => {
     

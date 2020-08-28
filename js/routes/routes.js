7f3da742e847af14a2dac6e69dc8e28c8860d744
Michael Smith <mijamessmith@gmail.com>
2020-08-28 11:47:40 -0700

    added password validation

--- js/routes/routes.js
+++ js/routes/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../controller/db")
-
+const bcrypt = require("bcryptjs")
 //middleware functions
 
 // checking if teachers are logged in 
@@ -68,6 +68,23 @@ isRegistered = function (req, res, next) {
     }
 }
 
+validEmail = function (req, res, next) {
+    try {
+        db.pool.query(`SELECT * FROM user_data WHERE email = "${req.body.email || req.query.email}"`, (error, results) => {
+            if (error) {
+                console.log(error);
+            } else if (results.length === 0) {
+                req.session.uniqueEmailForRegistration = true
+                next();
+            } else if (results.length > 0) {
+                req.session.uniqueEmailForRegistration = false
+                next();
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 
 router.get('/', function (req, res, next) {
@@ -83,27 +100,54 @@ router.get('/login', alreadyLoggedIn, loginMessage, function (req, res, next) {
 });
 
 router.post("/loginUser", async (req, res, next) => {
+    const { teacherEmail, teacherPassword } = req.body;
     var message;
     console.log(req.body)
     console.log(`Sent in a post using email: ${req.body.teacherEmail} and password: ${req.body.teacherPassword}.`)
     try {
-            db.pool.query(`SELECT * FROM user_data WHERE email = "${req.body.teacherEmail}" AND password = "${req.body.teacherPassword}"`, (error, results) => {
-                if (error) {
-                    console.log(error);
-                } else if (results.length === 0) {
-                    message = { message: "The email or password is incorrect" }
-                    console.log(message);
-                    res.status(401).render("pages/login", message)
-                } else if (results.length > 0) {               
-                    req.session.currentUser = results[0];  
+        db.pool.query(`SELECT * FROM user_data WHERE email = "${teacherEmail}"`, async (error, results) => {
+            if (error) {
+                console.log(error);
+            } else if (results.length === 0) {
+                message = { message: "That email is not registered" }
+                console.log(message);
+                res.render("pages/login", message)
+            } else if (results.length > 0) {
+                user = results[0]
+                const validPassword = await bcrypt.compare(teacherPassword, user.password) //returns true or false
+                if (validPassword) {
+                    req.session.currentUser = results[0];
                     console.log(req.session.currentUser, req.session.currentUserId)
                     res.redirect("/teachers");
+                } else {
+                    message = { message: "password incorrect" };
+                    res.render("pages/login", message);
                 }
-            })
+            }
+        })
     } catch (error) {
         console.log(error)
     }
 })
+
+
+//            db.pool.query(`SELECT * FROM user_data WHERE email = "${req.body.teacherEmail}" AND password = "${req.body.teacherPassword}"`, (error, results) => {
+//                if (error) {
+//                    console.log(error);
+//                } else if (results.length === 0) {
+//                    message = { message: "The email or password is incorrect" }
+//                    console.log(message);
+//                    res.status(401).render("pages/login", message)
+//                } else if (results.length > 0) {               
+//                    req.session.currentUser = results[0];  
+//                    console.log(req.session.currentUser, req.session.currentUserId)
+//                    res.redirect("/teachers");
+//                }
+//            })
+//    } catch (error) {
+//        console.log(error)
+//    }
+//})
 
 
 
@@ -111,36 +155,77 @@ router.post("/loginUser", async (req, res, next) => {
 
 router.get('/register', alreadyLoggedIn, function (req, res, next) {
     var message;
+    if (req.session.message) {
+        if (req.session.message.includes("did not match") || req.session.message.includes("already registered")) {
+            message = req.session.message;
+        }
+    }
     res.render('pages/register', {message: message});
 });
 
-router.post("/registerUser", isRegistered, (req, res, next) => {
-
-    const { email, password } = req.body;
-    console.log(email, password);
-
-
+router.post("/registerUser", async (req, res, next) => {
     try {
-        if (req.session.uniqueEmailForRegistration == true) {
-            db.pool.query(`INSERT INTO user_data (name, email, password) 
-    VALUES ("${req.body.name}", "${req.body.email}", "${req.body.password}")`, (err, result) => {
-                console.log(req.body)
-                if (err) throw err;
-                else {
-                    //set up an empty currentUser for the time being;
-                    req.session.currentUser = {};
-                    req.session.currentUser.messageAfterRegistering = `Welcome, ${req.body.name}. Login to access your new account`
-                    res.redirect("/login")
-                }
-            });
-        } else if (uniqueEmailForRegistration == false) {
-            res.session.message = "Email already registered"
-            res.redirect("/login")
+        let hasAccount;
+        const { email, password, confirmPassword, name } = req.body;
+        if (confirmPassword !== password) {
+            req.session.message = "passwords did not match"
+            return res.redirect("/register");
         }
-    } catch (error) {
-        console.log(error)
+        return db.pool.query(`SELECT * FROM user_data WHERE email = "${req.body.email || req.query.email}"`, async (error, results) => {
+            if (error) {
+                console.log(error);
+            } else if (results.length === 0) {
+                hasAccount = false
+            } else if (results.length > 0) {
+                hasAccount = true
+            } if (hasAccount) {
+                req.session.message = "An account with that email is already registered - Please Login"
+                res.redirect("/register")
+            } else if (!hasAccount) {
+                const hash = await bcrypt.hash(password, 10)
+                return db.pool.query(`INSERT INTO user_data (name, email, password) 
+    VALUES ("${name}", "${email}", "${hash}")`, (err, result) => {
+                    console.log(req.body)
+                    if (err) throw err;
+                    else {
+                        //set up an empty currentUser for the time being;
+                        req.session.currentUser = {};
+                        req.session.currentUser.messageAfterRegistering = `Welcome, ${req.body.name}. Login to access your new account`
+                        res.redirect("/login")
+                    }
+                });
+            }
+        })
+    } catch (e) {
+        if (e) throw e;
     }
 })
+
+
+
+
+
+//    try {
+//        if (req.session.uniqueEmailForRegistration == true) {
+//            db.pool.query(`INSERT INTO user_data (name, email, password) 
+//    VALUES ("${req.body.name}", "${req.body.email}", "${req.body.password}")`, (err, result) => {
+//                console.log(req.body)
+//                if (err) throw err;
+//                else {
+//                    //set up an empty currentUser for the time being;
+//                    req.session.currentUser = {};
+//                    req.session.currentUser.messageAfterRegistering = `Welcome, ${req.body.name}. Login to access your new account`
+//                    res.redirect("/login")
+//                }
+//            });
+//        } else if (uniqueEmailForRegistration == false) {
+//            res.session.message = "Email already registered"
+//            res.redirect("/login")
+//        }
+//    } catch (error) {
+//        console.log(error)
+//    }
+//})
 
 
 
